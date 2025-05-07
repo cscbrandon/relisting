896fd55daab6f7c281698a45316ee3736dab0d1f
cscbrandon <brandon.venom@gmail.com>
2025-05-07 17:21:44 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
